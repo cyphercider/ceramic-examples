900a0d7e57ec743809a39fbd4adad6338538aa5a
jeffkhull <jeffkhull@github>
2021-07-01 09:11:38 -0400

    add json validator example

--- src/pouchdb.ts
+++ src/pouchdb.ts
@@ -1,3 +1,6 @@
+const Ajv = require("ajv")
+const ajv = new Ajv()
+import { JSONSchema4 } from "json-schema"
 import PouchDB from "pouchdb"
 PouchDB.plugin(require("pouchdb-find"))
 import { ulid } from "ulid"
@@ -6,6 +9,26 @@ interface Dog {
   _id: string
   name: string
   age: number
+}
+
+const DogSchema: JSONSchema4 = {
+  title: "Dog",
+  type: "object",
+  required: ["_id", "name", "age"],
+  properties: {
+    _id: {
+      type: "string",
+      description: "primary key",
+    },
+    name: {
+      type: "string",
+      description: "Dog's name",
+    },
+    age: {
+      type: "number",
+      description: "dog's age",
+    },
+  },
 }
 
 async function testPouchdb(): Promise<void> {
@@ -17,26 +40,38 @@ async function testPouchdb(): Promise<void> {
     age: 5,
   }
 
-  const bo: Dog = {
-    _id: ulid(),
-    name: "Bo",
-    age: 9,
+  const missingAge = {
+    _id: "id",
   }
 
-  await db.createIndex({
-    index: { fields: ["name"] },
-  })
+  //   const bo: Dog = {
+  //     _id: ulid(),
+  //     name: "Bo",
+  //     age: 9,
+  //   }
 
-  db.put(max)
-  db.put(bo)
+  //   await db.createIndex({
+  //     index: { fields: ["name"] },
+  //   })
 
-  const res = await db.find({
-    selector: {
-      name: "Max",
-    },
-  })
+  //   db.put(max)
+  //   db.put(bo)
 
-  console.log(`res are `, res)
+  //   const res = await db.find({
+  //     selector: {
+  //       name: "Max",
+  //     },
+  //   })
+  //   const found = res.docs[0]
+
+  const validate = ajv.compile(DogSchema)
+  const valid = validate(max)
+  console.log(`valid`, valid)
+
+  //   const validationResult = validate(missingAge, DogSchema)
+  //   console.log(`validation result is `, validationResult)
+
+  //   console.log(`res are `, res.docs[0])
 }
 
 testPouchdb()
