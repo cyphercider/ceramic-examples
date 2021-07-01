53cc98a32d784a15f2af21d1fe4515b10411e3c0
jeffkhull <jeffkhull@github>
2021-07-01 08:31:34 -0400

    remove testdb

--- src/pouchdb.ts
+++ src/pouchdb.ts
@@ -1,10 +1,42 @@
 import PouchDB from "pouchdb"
+PouchDB.plugin(require("pouchdb-find"))
+import { ulid } from "ulid"
+
+interface Dog {
+  _id: string
+  name: string
+  age: number
+}
 
 async function testPouchdb(): Promise<void> {
   const db = new PouchDB("testdb")
 
-  const info = await db.info()
-  console.log(`info is`, info)
+  const max: Dog = {
+    _id: ulid(),
+    name: "Max",
+    age: 5,
+  }
+
+  const bo: Dog = {
+    _id: ulid(),
+    name: "Bo",
+    age: 9,
+  }
+
+  await db.createIndex({
+    index: { fields: ["name"] },
+  })
+
+  db.put(max)
+  db.put(bo)
+
+  const res = await db.find({
+    selector: {
+      name: "Max",
+    },
+  })
+
+  console.log(`res are `, res)
 }
 
 testPouchdb()
